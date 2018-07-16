14035ed714aed1cb98d5dbad963090d9b9d9f616
tanner00 <gasquakestudios@gmail.com>
2018-07-16 16:25:13 -0500

    a minimal version of grep that supports regexes

--- dgrep/source/app.d
+++ dgrep/source/app.d
@@ -1,6 +1,29 @@
+import std.conv;
+import std.file;
+import std.range : enumerate;
+import std.regex;
 import std.stdio;
 
-void main()
+import colorize : color, cwriteln, cwritef, fg;
+
+// G/re/p supported by Dlang regexes.
+// Example: ./dgrep source/app.d "(if|foreach)"
+
+// @todo: add switch for non-matching lines.
+// @todo: add more general pipes
+
+void main(string[] args)
 {
-	writeln("Edit source/app.d to start your project.");
+	assert(args.length == 3, "Please supply a filename and regex");
+
+        auto haystack = File(args[1]);
+        auto needle = regex(args[2], "m");
+	
+	foreach (i, l; haystack.byLine.enumerate(1)) {
+		auto m = matchFirst(l, needle);
+		if (m.length == 0) continue;
+		
+		cwritef("%03d: ".color(fg.yellow), i);
+		cwriteln(to!string(l).color(fg.light_green));
+	}
 }
