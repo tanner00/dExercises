0286c248c4ca19089aaf5a0027c085e106f1ea54
tanner00 <gasquakestudios@gmail.com>
2018-07-16 14:49:18 -0500

    dhexdump complete (unless I learn something later to make this cleaner)

--- dhexdump/source/app.d
+++ dhexdump/source/app.d
@@ -1,6 +1,30 @@
 import std.stdio;
+import std.algorithm.iteration : map;
 
-void main()
+// Tested on some ELF files in anu/build
+
+void main(string[] args)
 {
-	writeln("Edit source/app.d to start your project.");
+	assert(args.length == 2, "Please supply a filename to read");
+
+	auto f = File(args[1]);
+	scope(exit) f.close();
+
+	const read_size = 0x10;
+	
+	auto n = 0;
+	foreach (ubyte[] line; f.byChunk(read_size)) {
+		auto text_repr = line.map!(c =>
+					   c > '~' || c < ' ' ? '.' : char(c));
+
+		// The hard bits of this format string is from dlang.org landing page.
+		// They seem quite powerful in D.
+		// The number 3 is used in 3 * (read_size) - line.length because
+		// each printed byte int hex including the space will be of length 3.
+		writefln!("%08x: %(%02x %)%*s  %s")(n, line, 3 *
+						   (read_size - line.length), "", text_repr);
+		
+	        n += read_size;
+	}
+	
 }
