3520663740653cf9b57876797a1b66f9f0bc03f5
tanner00 <gasquakestudios@gmail.com>
2018-07-31 16:53:59 -0500

    clean up hll

--- dHyperLogLog/source/app.d
+++ dHyperLogLog/source/app.d
@@ -19,32 +19,29 @@ void main()
         return vs;
     }
 
-    enum t = 25;
-    float avgError = 0;
+    enum t = 10;
     foreach (i; 0 .. t)
     {
-        auto n = uniform(1, 8000000);
-        auto cut = 10;
+        auto n = uniform(1, 50_000_000);
+        enum cut = 5;
 
         auto values = randInts(n);
         int[] buckets;
-        buckets.length = 1536 / 4; // 1.5 KiB
+        buckets.length = 1000;
 
-        import std.math : log2, pow;
-
-		// Traverse into the middle of the data stream for fun.
-		auto testCase = values.drop(cast(int) n.log2).takeExactly(n / cut);
+        // Traverse into the middle of the data stream for fun.
+        auto testCase = values.drop(n / cut).takeExactly(n / cut);
         foreach (v; testCase)
         {
             auto bn = v.hashOf % buckets.length;
             buckets[bn] = max(buckets[bn], v.bsf);
         }
 
+		import std.math : log2, pow;
         auto est = 2 ^^ (cast(double) buckets.sum / buckets.length) * buckets
             .length * 0.79402 * cut;
-        // writefln("Exact: %s | Est: %d", n, cast(int) est);
-        // writeln("Error: ", n / est);
-        avgError += n / est;
+
+		auto e = est / n;
+        writefln("Exact: %s\tEst: %d\tError: %s", n, cast(int) est, e);
     }
-    writefln("%.3f", avgError / t);
 }
